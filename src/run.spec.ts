f290e8a4fbaa7e5f84d0e1760eda8d1c2cccac50
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-04-04 00:11:01 +0200

    feat: initial commit

---
+++ src/run.spec.ts
@@ -0,0 +1,79 @@
+import { describe, it, mock } from 'node:test'
+import run from './run.js'
+import assert from 'node:assert/strict'
+import os from 'node:os'
+
+void describe('run()', () => {
+	void it('should run a command and return the output', async () => {
+		const res = await run({
+			command: 'echo',
+			args: ['-n', 'test'],
+		})
+
+		assert.equal(res, 'test')
+	})
+
+	void it('should pass input', async () => {
+		const res = await run({
+			command: '/bin/bash',
+			args: ['-c', 'read input && echo $input'],
+			input: `test${os.EOL}`,
+		})
+		assert.equal(res, `test${os.EOL}`)
+	})
+
+	void it('should log the executed command', async () => {
+		const debug = mock.fn()
+		await run({
+			command: 'echo',
+			args: ['-n', 'test'],
+			log: {
+				debug,
+			},
+		})
+		assert.equal(debug.mock.calls[0]?.arguments[0], 'echo -n test')
+	})
+
+	void it('should reject if the command does not exist', async () =>
+		assert.rejects(
+			async () =>
+				run({
+					command: 'foo',
+				}),
+			/spawn foo ENOENT/,
+		))
+
+	void it('should reject on non-zero exit code', async () =>
+		assert.rejects(
+			async () =>
+				run({
+					command: '/bin/bash',
+					args: ['-c', 'exit 42'],
+				}),
+			/\(42\)/,
+		))
+
+	void it('should log the errors', async () => {
+		const error = mock.fn()
+		await run({
+			command: 'npx',
+			args: ['tsx', 'src/test/stderr.ts'],
+			log: {
+				stderr: error,
+			},
+		})
+		assert.equal(error.mock.calls[0]?.arguments[0].toString(), 'error')
+	})
+
+	void it('should log the output', async () => {
+		const log = mock.fn()
+		await run({
+			command: 'echo',
+			args: ['-n', 'test'],
+			log: {
+				stdout: log,
+			},
+		})
+		assert.equal(log.mock.calls[0]?.arguments[0].toString(), 'test')
+	})
+})
---
+++ src/run.ts
@@ -0,0 +1,49 @@
+import { spawn } from 'node:child_process'
+import os from 'node:os'
+
+const run = async ({
+	command,
+	args,
+	input,
+	log,
+}: {
+	command: string
+	args?: string[]
+	input?: string
+	log?: {
+		debug?: (...message: any[]) => void
+		stdout?: (...message: any[]) => void
+		stderr?: (...message: any[]) => void
+	}
+	onError?: (code: number) => void
+}): Promise<string> =>
+	new Promise((resolve, reject) => {
+		log?.debug?.(`${command} ${args?.join(' ')}`)
+		const p = spawn(command, args)
+		const result = [] as string[]
+		const errors = [] as string[]
+		if (input !== undefined) {
+			p.stdin.write(input)
+		}
+		p.on('close', (code) => {
+			if (code !== 0) {
+				return reject(
+					new Error(
+						`${command} ${args?.join(' ')} failed (${code}): ${errors.join(os.EOL)}`,
+					),
+				)
+			}
+			return resolve(result.join(os.EOL))
+		})
+		p.stdout.on('data', (data) => {
+			result.push(data)
+			log?.stdout?.(data)
+		})
+		p.stderr.on('data', (data) => {
+			errors.push(data)
+			log?.stderr?.(data)
+		})
+		p.on('error', reject)
+	})
+
+export default run
